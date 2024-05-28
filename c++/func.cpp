d86b06e65230ac5c7ecec449463a2492eb4bb941
uria melaku <157289857+uriamelaku@users.noreply.github.com>
2024-05-28 17:49:44 +0300

    Add files via upload

---
+++ c++/func.cpp
@@ -0,0 +1,5 @@
+#include "func.hpp"
+
+int sum(int a, int b){
+    return a+b;
+}
---
+++ c++/main.cpp
@@ -0,0 +1,10 @@
+//#include <iostream>
+#include "func.hpp"
+
+int main() {
+    int a = 5;
+    int b = 4;
+    sum(a, b);
+    //std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
